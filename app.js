8f22f1d18ec3d84ab39effdb1dfe4a322c4eab6e
adamgajzlerowicz <nelf86@gmail.com>
2018-03-26 22:12:42 +0200

    replacing words from real api!

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const word = 'react';
+const word = 'uncanny';
 
-const replaceText = (text, synonims)=>{
-    return text;
-}
-const getChildNodes = (node, synonims)=>{
+
+const replaceText = (text, synonyms) => {
+    return text.replace(new RegExp(synonyms.join('|'), 'ig'), word);
+};
+const getChildNodes = (node, synonyms)=>{
     if (node.hasChildNodes()) {
         const children = node.childNodes;
 
         for (let i = 0; i < children.length; i++) {
-            getChildNodes(children[i])
+            getChildNodes(children[i], synonyms)
         }
+
     } else {
         if(node.textContent){
-            node.textContent = replaceText(node.textContent, synonims)
+            node.textContent = replaceText(node.textContent, synonyms);
         }
     }
-}
+};
 
 fetch('https://words.bighugelabs.com/api/2/2907f195ac339d6cc7021a2280796049/'+ word +'/json')
     .then(r=>r.json())
-    .then(synonims=>{
-        const verbs = synonims.verb ? synonims.verb.syn : [];
-        const nouns = synonims.noun ? synonims.noun.syn : [];
-        const allWords = verbs.concat(nouns);
-        getChildNodes(document, synonims)
+    .then(synonyms=>{
+        const verbs = synonyms.verb ? synonyms.verb.syn : [];
+        const adjective = synonyms.adjective ? synonyms.adjective.syn : [];
+        const nouns = synonyms.noun ? synonyms.noun.syn : [];
+        const allWords = adjective.concat(verbs.concat(nouns));
+        allWords && getChildNodes(document.body, allWords);
     });
